f2d104f3f67fc2a76d8167f69633da4e5d9191df
unknown <ntninitin@gmail.com>
2019-01-01 22:55:39 +0530

    (added)

---
+++ runner/3d_runner/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    Transform player;
+
+    [SerializeField]
+    float smoothRate;
+
+    Vector3 offset;
+    void Start()
+    {
+        offset = player.position - transform.position;   
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 currentpos = transform.position;
+        Vector3 newpos = player.position - offset;
+
+        transform.position = Vector3.Lerp(currentpos, newpos, smoothRate);
+    }
+}
