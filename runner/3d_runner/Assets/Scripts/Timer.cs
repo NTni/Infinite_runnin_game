5d7da6a36eea8a2cdb4411bcd625d2989ff3af84
unknown <ntninitin@gmail.com>
2019-01-13 22:33:44 +0530

    (updateadded)

---
+++ runner/3d_runner/Assets/Scripts/Timer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField]
+    public float RunEvery = 1;
+    public bool RunOnAwake = false;
+    public bool Loop = true;
+    public UnityEvent OnTimerEnd;
+
+    void OnEnable()
+    {
+        if (RunOnAwake)
+        {
+            Invoke("Execute", 0);
+        }
+
+        if (Loop)
+        {
+            InvokeRepeating("Execute", RunEvery, RunEvery);
+        }
+        else
+        {
+            Invoke("Execute", RunEvery);
+        }
+    }
+
+    public void Execute()
+    {
+        OnTimerEnd.Invoke();
+    }
+}
